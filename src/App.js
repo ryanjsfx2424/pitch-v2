3f48e8455d2004806124ad43a984b6c01324dd06
Ryan Farber <ryanjsfx24@gmail.com>
2022-11-29 00:26:44 +0100

    mobile + desktop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './pitch.svg';
+import mobile from './mobile.svg';
+import desktop from './desktop.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(0)
+  const [windowHeight, setWindowHeight] = useState(1)
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <a href="https://discord.gg/8rDC46THMN">
-      <img src={logo} className="background-img" alt="background image" />
+      {(windowWidth > windowHeight) ?
+        <img src={desktop} className="background-img" alt="background image"/>
+      :
+        <img src={mobile} className="background-img" alt="background image"/>
+      }
     </a>
   );
 }
